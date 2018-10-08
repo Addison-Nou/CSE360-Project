78baab8bf1556223a870f56ed5d1bcef2dc9c191
DESKTOP-77V002R\conno <caweese@asu.edu>
2018-10-08 14:35:54 -0700

    Added the SearchForHead and SearchForDependencies function, as well as added setters to the Node class and a few other fixes to the Main

--- src/cse360proj/CSE360Proj/Main.java
+++ src/cse360proj/CSE360Proj/Main.java
@@ -18,45 +18,45 @@ public class Main {
     	
     	String temp = "";
     	
-    	String output;
     	boolean cont = true;
     	
     	ArrayList<String> A = new ArrayList<String>();
     	ArrayList<Integer> B = new ArrayList<Integer>();
     	
     	ArrayList<Node> N = new ArrayList<Node>();
-    	ArrayList<String> dependencies = new ArrayList<String>();
     	
     	//1 to continue, 2 to exit
         
     	while (cont) {
-    		if (!dependencies.isEmpty()) dependencies.clear();
+    		Node holder = new Node();
     		
     		System.out.print("Please enter a name: ");
     		name = s.nextLine();
+    		holder.setName(name);
 
     		System.out.print("Please enter a duration: ");
     		duration = s.nextInt();
+    		holder.setDuration(duration);
     		
     		System.out.print("Please enter any dependencies ('e' to quit): ");
     		temp = s.next();
     		
-    		while (!temp.equals("potato")) {
-    			dependencies.add(temp);
-    			System.out.println("Current dependencies: " + dependencies);
+    		while (!temp.equals("e")) {
+    			holder.addDependencies(temp);
+    			System.out.println("Current dependencies: " + holder.getDependencies());
         		temp = s.next();
     		}
     		
     		System.out.println("\nName: " + name);
     		System.out.println("Duration: " + duration);
-    		System.out.println("Dependencies: " + dependencies);
+    		System.out.println("Dependencies: " + holder.getDependencies());
     		
     		//Add data to array
     		A.add(name);
     		B.add(duration);
     		
     		//Add data to node and add new node to ArrayList
-    		N.add(new Node(name, duration, dependencies));
+    		N.add(holder);
     		
     		//Check for continue
     		System.out.println("Do you wish to continue? 1 for yes 2 for no");
@@ -64,10 +64,45 @@ public class Main {
     			
     			//System.out.println("Names: " + A);
     			//System.out.println("Durations: " + B);
-    			
+    			Node head = SearchForHead(N);
+    			Node inPath = SearchForDependencies(N, head.getName());
+    	    	System.out.println("Head: " + head.getName());
+    	    	System.out.println("In Path: " + inPath.getName());
     			return;
     		} else s.nextLine();
+    		
     	}
+    	s.close();
+    }
+    
+    public static Node SearchForHead(ArrayList<Node> list) {
+    	
+    	Node head = new Node();
+    	
+    	for(int i=0; i<list.size(); i++) {
+    		
+    		if(list.get(i).getDependencies().isEmpty()) {
+    			head = list.get(i);
+    			return head;
+    		}
+    	}
+    	
+    	return head;
+    }
+    
+    public static Node SearchForDependencies(ArrayList<Node> list, String dependency) {
+    	
+    	Node temp = new Node();
+    	
+		for(int i=0; i<list.size(); i++) {
+			
+			ArrayList<String> tmp = list.get(i).getDependencies();
+		    if(tmp.contains(dependency)) {
+		    	temp = list.get(i);
+		    }
+    	}
+		
+		return temp;
     }
     
 }
--- src/cse360proj/CSE360Proj/Node.java
+++ src/cse360proj/CSE360Proj/Node.java
@@ -8,6 +8,12 @@ public class Node {
 	int duration;
 	ArrayList<String> dependencies = new ArrayList<String>();
 	
+	public Node() {
+		this.name = "";
+		this.duration = 0;
+		this.dependencies = new ArrayList<String>();
+	}
+	
 	public Node(String name, int duration, ArrayList<String> dependencies) {
 		this.name = name;
 		this.duration = duration;
@@ -25,6 +31,23 @@ public class Node {
 	
 	public ArrayList<String> getDependencies() {
 		return dependencies;
+	}
+	
+	//Setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+	
+	public void setDependencies(ArrayList<String> dependencies) {
+		this.dependencies = dependencies;
+	}
+	
+	public void addDependencies(String dependency) {
+		this.dependencies.add(dependency);
 	}
 
 }
