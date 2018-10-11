b36fffdbdc63d96ca796dd917602f28d3ad03b84
zsomers <zsomers1@asu.edu>
2018-10-11 00:09:45 -0700

    new GUI now based on new mock up with 80% more window and everyones favorite font buy now before its out of stock

--- src/GUI.java
+++ src/GUI.java
@@ -5,6 +5,8 @@ import javax.swing.JTabbedPane;
 import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextArea;
 import javax.swing.JScrollBar;
@@ -12,10 +14,16 @@ import javax.swing.JButton;
 import javax.swing.SwingConstants;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JTextField;
 
 public class GUI {
 
-	private JFrame frame;
+	private JFrame frmCseProject;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -25,7 +33,7 @@ public class GUI {
 			public void run() {
 				try {
 					GUI window = new GUI();
-					window.frame.setVisible(true);
+					window.frmCseProject.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -44,12 +52,13 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmCseProject = new JFrame();
+		frmCseProject.setTitle("CSE 360 Project");
+		frmCseProject.setBounds(100, 100, 722, 480);
+		frmCseProject.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
-		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+		frmCseProject.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
 		tabbedPane.addTab("Input", null, panel, null);
@@ -57,46 +66,40 @@ public class GUI {
 		
 		JButton button = new JButton("Help");
 		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {    //this is the button(action) that corresponds with the "Help" Button 
-			}                                                 //TODO add page for help section as the button action
+			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "Help" Button 
+				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
+			}                                                    //TODO add filler text to the Pop-up as the button action
 		});
 		button.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		button.setBounds(295, 1, 62, 23);
+		button.setBounds(564, 1, 62, 23);
 		panel.add(button);
 		
 		JButton button_1 = new JButton("About");
 		button_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {    //this is the button(action) that corresponds with the "About" Button 
-			}                                                 //TODO add page for about section as the button action
+			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "About" Button
+				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
+			}                                                    //TODO add filler text to the Pop-up as the button action
 		});
 		button_1.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		button_1.setBounds(357, 1, 72, 23);
+		button_1.setBounds(627, 1, 72, 23);
 		panel.add(button_1);
-		
-		JTextArea textArea_2 = new JTextArea();
-		textArea_2.setBounds(203, 51, 170, 136);
-		panel.add(textArea_2);
-		
-		JScrollBar scrollBar_3 = new JScrollBar();
-		scrollBar_3.setBounds(375, 51, 17, 136);
-		panel.add(scrollBar_3);
 		
 		JLabel lblActivityName = new JLabel("Activity Name");
 		lblActivityName.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblActivityName.setBounds(10, 28, 98, 14);
+		lblActivityName.setBounds(80, 40, 100, 25);
 		panel.add(lblActivityName);
 		
 		JTextArea textArea_3 = new JTextArea();
-		textArea_3.setBounds(10, 51, 158, 22);
+		textArea_3.setBounds(80, 70, 150, 25);
 		panel.add(textArea_3);
 		
 		JLabel lblDuration = new JLabel("Duration");
 		lblDuration.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblDuration.setBounds(10, 84, 98, 14);
+		lblDuration.setBounds(80, 120, 100, 25);
 		panel.add(lblDuration);
 		
 		JTextArea textArea_4 = new JTextArea();
-		textArea_4.setBounds(10, 108, 158, 22);
+		textArea_4.setBounds(80, 150, 150, 25);
 		panel.add(textArea_4);
 		
 		JButton btnProcess = new JButton("PROCESS");
@@ -104,23 +107,22 @@ public class GUI {
 			public void actionPerformed(ActionEvent arg0) {    //this is the button(action) that corresponds with the "PROCESS" Button 
 			}
 		});
-		btnProcess.setHorizontalAlignment(SwingConstants.LEFT);
-		btnProcess.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnProcess.setBounds(10, 154, 95, 23);
+		btnProcess.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnProcess.setBounds(220, 325, 110, 35);
 		panel.add(btnProcess);
 		
-		JButton btnAddNewPredecessors = new JButton("Add New Predecessors");
+		JButton btnAddNewPredecessors = new JButton("Add Predecessor");
 		btnAddNewPredecessors.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {      //this is the button(action) that corresponds with the "Add new Predecessor" Button
+			public void actionPerformed(ActionEvent e) {      //this is the button(action) that corresponds with the "Add Predecessor" Button
 			}
 		});
-		btnAddNewPredecessors.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		btnAddNewPredecessors.setBounds(203, 199, 170, 23);
+		btnAddNewPredecessors.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnAddNewPredecessors.setBounds(420, 150, 170, 25);
 		panel.add(btnAddNewPredecessors);
 		
-		JLabel lblPredecessorList = new JLabel("Predecessor List");
+		JLabel lblPredecessorList = new JLabel("Predecessor");
 		lblPredecessorList.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblPredecessorList.setBounds(204, 35, 114, 14);
+		lblPredecessorList.setBounds(420, 70, 120, 25);
 		panel.add(lblPredecessorList);
 		
 		JButton btnRestart = new JButton("RESTART");
@@ -128,62 +130,59 @@ public class GUI {
 			public void actionPerformed(ActionEvent arg0) {     //this is the button(action) that corresponds with the "RESTART" Button 
 			}
 		});
-		btnRestart.setHorizontalAlignment(SwingConstants.LEFT);
-		btnRestart.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnRestart.setBounds(10, 187, 97, 23);
+		btnRestart.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnRestart.setBounds(400, 325, 110, 35);
 		panel.add(btnRestart);
+		
+		textField = new JTextField();
+		textField.setBounds(420, 100, 200, 25);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("Add Node");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {      //this is the button(action) that corresponds with the "Add Node" Button
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnNewButton_1.setBounds(80, 200, 150, 25);
+		panel.add(btnNewButton_1);
 		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Output", null, panel_1, null);
 		panel_1.setLayout(null);
 		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportBorder(new LineBorder(new Color(0, 0, 0)));
+		scrollPane.getViewport().setBackground(Color.white);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setBounds(80, 100, 480, 240);
+		panel_1.add(scrollPane);
+		
 		JLabel lblActivityList = new JLabel("Activity List");
 		lblActivityList.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblActivityList.setBounds(10, 36, 108, 24);
+		lblActivityList.setBounds(80, 75, 100, 25);
 		panel_1.add(lblActivityList);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(10, 61, 178, 146);
-		panel_1.add(textArea);
-		
-		JScrollBar scrollBar = new JScrollBar();
-		scrollBar.setBounds(188, 61, 17, 153);
-		panel_1.add(scrollBar);
-		
-		JLabel lblAc = new JLabel("Activity Diagram");
-		lblAc.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblAc.setBounds(226, 36, 125, 22);
-		panel_1.add(lblAc);
 		
 		JButton btnNewButton = new JButton("Help");
 		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {      //this is the button(action) that corresponds with the "Help" Button
-			}                                                 //TODO add page for help section as the button action
+			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "Help" Button
+				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
+			}                                                    //TODO add filler text to the Pop-up as the button action
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		btnNewButton.setBounds(295, -1, 62, 23);
+		btnNewButton.setBounds(565, -1, 62, 23);
 		panel_1.add(btnNewButton);
 		
 		JButton btnAbout = new JButton("About");
 		btnAbout.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {      //this is the button(action) that corresponds with the "About" Button
-			}                                                 //TODO add page for about section as the button action
+			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "About" Button
+				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
+			}                                                    //TODO add filler text to the Pop-up as the button action
 		});
 		btnAbout.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		btnAbout.setBounds(357, -1, 72, 23);
+		btnAbout.setBounds(627, -1, 72, 23);
 		panel_1.add(btnAbout);
-		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(215, 61, 178, 146);
-		panel_1.add(textArea_1);
-		
-		JScrollBar scrollBar_1 = new JScrollBar();
-		scrollBar_1.setBounds(391, 61, 17, 153);
-		panel_1.add(scrollBar_1);
-		
-		JScrollBar scrollBar_2 = new JScrollBar();
-		scrollBar_2.setOrientation(JScrollBar.HORIZONTAL);
-		scrollBar_2.setBounds(215, 208, 178, 14);
-		panel_1.add(scrollBar_2);
 	}
 }
