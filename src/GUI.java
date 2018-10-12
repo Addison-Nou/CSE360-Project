4e9cea85c7fb6a5e69f05d0ae1ff421c9b33d43c
zsomers <zsomers1@asu.edu>
2018-10-12 00:25:28 -0700

    needs backend functionallity but is set up for code, Added Help and About text and made the appearance changes

--- src/GUI.java
+++ src/GUI.java
@@ -19,11 +19,14 @@ import javax.swing.ScrollPaneConstants;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
 import javax.swing.JTextField;
+import javax.swing.DropMode;
 
 public class GUI {
 
 	private JFrame frmCseProject;
-	private JTextField textField;
+	private JTextField PredecessorField;
+	private JTextField ActivityField;
+	private JTextField DurationField;
 
 	/**
 	 * Launch the application.
@@ -64,56 +67,59 @@ public class GUI {
 		tabbedPane.addTab("Input", null, panel, null);
 		panel.setLayout(null);
 		
-		JButton button = new JButton("Help");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "Help" Button 
-				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
-			}                                                    //TODO add filler text to the Pop-up as the button action
+		JButton HelpInput = new JButton("Help");
+		HelpInput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {         	//this is the button(action) that corresponds with the "Help" Button 
+				JOptionPane.showMessageDialog(null, 
+						"\t\tHELP\t\t\n"
+						+ "The Application can be exited by the red X in the upper right corner\n"
+						+ "The \"Add Node\" button  takes the Activity Name and Duration and Creates a new node\n"
+						+ "The \"Process\" button creates paths based on existing nodes\n"
+						+ "The \"Add Predecessor\" button adds a predecessor to the current node\n"
+						+ "The \"Restart\" button resets the application to a blank slate");    	//help copy pasta
+			}                                                    	//TODO add filler text to the Pop-up as the button action
 		});
-		button.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		button.setBounds(564, 1, 62, 23);
-		panel.add(button);
+		HelpInput.setFont(new Font("Tahoma", Font.ITALIC, 12));
+		HelpInput.setBounds(564, 1, 62, 23);
+		panel.add(HelpInput);
 		
-		JButton button_1 = new JButton("About");
-		button_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "About" Button
-				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
-			}                                                    //TODO add filler text to the Pop-up as the button action
+		JButton AboutInput = new JButton("About");
+		AboutInput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {         	//this is the button(action) that corresponds with the "About" Button
+				JOptionPane.showMessageDialog(null, 
+						"Authors: Addison Nou, Connor Weese, Muhammed Kilig, Zachary Somers\n"
+						+ "This program analyzes a given network diagram and determines all paths within the network.\n"
+						+ "It recieves input of Activity Name, Duration and List of Predecessors with no limit to activities or predecessors.\n"
+						+ "It ouputs a list of activity paths and durations listed in descending order according to the value durations.\n"
+						+ "CSE 360 Fall 2018");    	//about copy pasta
+			}                                                		//TODO add filler text to the Pop-up as the button action
 		});
-		button_1.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		button_1.setBounds(627, 1, 72, 23);
-		panel.add(button_1);
+		AboutInput.setFont(new Font("Tahoma", Font.ITALIC, 12));
+		AboutInput.setBounds(627, 1, 72, 23);
+		panel.add(AboutInput);
 		
 		JLabel lblActivityName = new JLabel("Activity Name");
 		lblActivityName.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblActivityName.setBounds(80, 40, 100, 25);
+		lblActivityName.setBounds(80, 40, 200, 25);
 		panel.add(lblActivityName);
-		
-		JTextArea textArea_3 = new JTextArea();
-		textArea_3.setBounds(80, 70, 150, 25);
-		panel.add(textArea_3);
 		
 		JLabel lblDuration = new JLabel("Duration");
 		lblDuration.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblDuration.setBounds(80, 120, 100, 25);
+		lblDuration.setBounds(80, 120, 200, 25);
 		panel.add(lblDuration);
-		
-		JTextArea textArea_4 = new JTextArea();
-		textArea_4.setBounds(80, 150, 150, 25);
-		panel.add(textArea_4);
 		
 		JButton btnProcess = new JButton("PROCESS");
 		btnProcess.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {    //this is the button(action) that corresponds with the "PROCESS" Button 
+			public void actionPerformed(ActionEvent arg0) {			//this is the button(action) that corresponds with the "PROCESS" Button 
 			}
 		});
 		btnProcess.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnProcess.setBounds(220, 325, 110, 35);
+		btnProcess.setBounds(200, 325, 150, 35);
 		panel.add(btnProcess);
 		
 		JButton btnAddNewPredecessors = new JButton("Add Predecessor");
 		btnAddNewPredecessors.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {      //this is the button(action) that corresponds with the "Add Predecessor" Button
+			public void actionPerformed(ActionEvent e) {			//this is the button(action) that corresponds with the "Add Predecessor" Button
 			}
 		});
 		btnAddNewPredecessors.setFont(new Font("Tahoma", Font.BOLD, 15));
@@ -122,67 +128,101 @@ public class GUI {
 		
 		JLabel lblPredecessorList = new JLabel("Predecessor");
 		lblPredecessorList.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblPredecessorList.setBounds(420, 70, 120, 25);
+		lblPredecessorList.setBounds(420, 70, 200, 25);
 		panel.add(lblPredecessorList);
 		
 		JButton btnRestart = new JButton("RESTART");
 		btnRestart.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {     //this is the button(action) that corresponds with the "RESTART" Button 
+			public void actionPerformed(ActionEvent arg0) {			//this is the button(action) that corresponds with the "RESTART" Button 
 			}
 		});
 		btnRestart.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnRestart.setBounds(400, 325, 110, 35);
+		btnRestart.setBounds(400, 325, 150, 35);
 		panel.add(btnRestart);
 		
-		textField = new JTextField();
-		textField.setBounds(420, 100, 200, 25);
-		panel.add(textField);
-		textField.setColumns(10);
+		PredecessorField = new JTextField();
+		PredecessorField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {			// listener for event(Predecessor field)
+			}
+		});
+		PredecessorField.setBounds(420, 100, 200, 25);
+		panel.add(PredecessorField);
+		PredecessorField.setColumns(10);
 		
 		JButton btnNewButton_1 = new JButton("Add Node");
 		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {      //this is the button(action) that corresponds with the "Add Node" Button
+			public void actionPerformed(ActionEvent arg0) {			//this is the button(action) that corresponds with the "Add Node" Button
 			}
 		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 15));
 		btnNewButton_1.setBounds(80, 200, 150, 25);
 		panel.add(btnNewButton_1);
 		
+		ActivityField = new JTextField();
+		ActivityField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {			// listener for event(Activity field)
+			}
+		});
+		ActivityField.setBounds(80, 70, 150, 25);
+		panel.add(ActivityField);
+		ActivityField.setColumns(10);
+		
+		DurationField = new JTextField();
+		DurationField.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {			// listener for event(Duration field)
+			}
+		});
+		DurationField.setBounds(80, 150, 150, 25);
+		panel.add(DurationField);
+		DurationField.setColumns(10);
+		
 		JPanel panel_1 = new JPanel();
 		tabbedPane.addTab("Output", null, panel_1, null);
 		panel_1.setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setViewportBorder(new LineBorder(new Color(0, 0, 0)));
-		scrollPane.getViewport().setBackground(Color.white);
-		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setBounds(80, 100, 480, 240);
-		panel_1.add(scrollPane);
 		
 		JLabel lblActivityList = new JLabel("Activity List");
 		lblActivityList.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblActivityList.setBounds(80, 75, 100, 25);
 		panel_1.add(lblActivityList);
 		
-		JButton btnNewButton = new JButton("Help");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "Help" Button
-				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
-			}                                                    //TODO add filler text to the Pop-up as the button action
+		JButton HelpOutput = new JButton("Help");
+		HelpOutput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {         	//this is the button(action) that corresponds with the "Help" Button
+				JOptionPane.showMessageDialog(null, 
+						"\t\tHELP\t\t\n"
+						+ "The Application can be exited by the red X in the upper right corner\n"
+						+ "The \"Add Node\" button  takes the Activity Name and Duration and Creates a new node\n"
+						+ "The \"Process\" button creates paths based on existing nodes\n"
+						+ "The \"Add Predecessor\" button adds a predecessor to the current node\n"
+						+ "The \"Restart\" button resets the application to a blank slate");    	//help copy pasta
+			}                                                    	//TODO add filler text to the Pop-up as the button action
 		});
-		btnNewButton.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		btnNewButton.setBounds(565, -1, 62, 23);
-		panel_1.add(btnNewButton);
+		HelpOutput.setFont(new Font("Tahoma", Font.ITALIC, 12));
+		HelpOutput.setBounds(565, -1, 62, 23);
+		panel_1.add(HelpOutput);
 		
-		JButton btnAbout = new JButton("About");
-		btnAbout.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {         //this is the button(action) that corresponds with the "About" Button
-				JOptionPane.showMessageDialog(null, "Popup");    //Creates Pop-up with text
-			}                                                    //TODO add filler text to the Pop-up as the button action
+		JButton AboutOutput = new JButton("About");
+		AboutOutput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {         	//this is the button(action) that corresponds with the "About" Button
+				JOptionPane.showMessageDialog(null, 
+						"Authors: Addison Nou, Connor Weese, Muhammed Kilig, Zachary Somers\n"
+						+ "This program analyzes a given network diagram and determines all paths within the network.\n"
+						+ "It recieves input of Activity Name, Duration and List of Predecessors with no limit to activities or predecessors.\n"
+						+ "It ouputs a list of activity paths and durations listed in descending order according to the value durations.\n"
+						+ "CSE 360 Fall 2018");    	//about copy pasta
+			}                                                    	//TODO add filler text to the Pop-up as the button action
 		});
-		btnAbout.setFont(new Font("Tahoma", Font.ITALIC, 12));
-		btnAbout.setBounds(627, -1, 72, 23);
-		panel_1.add(btnAbout);
+		AboutOutput.setFont(new Font("Tahoma", Font.ITALIC, 12));
+		AboutOutput.setBounds(627, -1, 72, 23);
+		panel_1.add(AboutOutput);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(80, 100, 520, 240);
+		panel_1.add(scrollPane);
+		
+		JTextArea ListArea = new JTextArea();
+		scrollPane.setViewportView(ListArea);
+		ListArea.setEditable(false);
+		ListArea.append(null); 										//This line is for outputtin to the ListArea
 	}
 }
